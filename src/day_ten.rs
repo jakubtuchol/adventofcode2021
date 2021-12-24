a79476c9e175ea5b62a83b96de825c9569b1b9d8
Jakub Tucholski <jtucholski00@gmail.com>
2021-12-23 17:13:33 -0800

    checkpointing work on day 10

--- src/day_ten.rs
+++ src/day_ten.rs
@@ -27,9 +27,67 @@ pub fn check_balanced(s: &str) -> usize {
     0
 }
 
+pub fn find_completion(s: &str) -> usize {
+    let mut completion: Vec<char> = Vec::new();
+    let mut stack: Vec<char> = Vec::new();
+    let close_matchings = HashMap::from([
+        (')', '('),
+        (']', '['),
+        ('}', '{'),
+        ('>', '<'),
+    ]);
+    let open_matchings = HashMap::from([
+        ('(', ')'),
+        ('[', ']'),
+        ('{', '}'),
+        ('<', '>'),
+    ]);
+
+    for c in s.chars() {
+        if close_matchings.contains_key(&c) {
+            if stack.is_empty() {
+                return 0;
+            }
+            let open = close_matchings.get(&c).unwrap();
+            let mut last = stack.pop().unwrap();
+        }
+        /*
+        if matching_closes.contains_key(&c) {
+            let (matching, value) = matching_closes.get(&c).unwrap();
+            if stack.is_empty() {
+                return *value;
+            } else {
+                let last_match = stack.pop().unwrap();
+                if last_match != *matching {
+                    return *value;
+                }
+            }
+        } else {
+            stack.push(c);
+        }
+ */
+    }
+    0
+
+}
+
+pub fn get_completion_score(s: &str) -> usize {
+    let points = HashMap::from([
+        (')', 1),
+        (']', 2),
+        ('}', 3),
+        ('>', 4),
+    ]);
+
+    s.chars().fold(0, |acc, x| {
+        let value = points.get(&x).unwrap();
+        acc * 5 + value
+    })
+}
+
 #[cfg(test)]
 mod test {
-    use super::check_balanced;
+    use super::{check_balanced, get_completion_score};
 
     #[test]
     fn test_check_balanced() {
@@ -39,5 +97,14 @@ mod test {
         assert_eq!(3, check_balanced("[<(<(<(<{}))><([]([]()"));
         assert_eq!(25137, check_balanced("<{([([[(<>()){}]>(<<{{"));
         assert_eq!(0, check_balanced("[({(<(())[]>[[{[]{<()<>>"));
+    }
+
+    #[test]
+    fn test_completion_score() {
+        assert_eq!(294, get_completion_score("])}>"));
+        assert_eq!(288957, get_completion_score("}}]])})]"));
+        assert_eq!(5566, get_completion_score(")}>]})"));
+        assert_eq!(1480781, get_completion_score("}}>}>))))"));
+        assert_eq!(995444, get_completion_score("]]}}]}]}>"));
     }
 }
