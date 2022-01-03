6ed6131f1f1c2068cb17711c5c152a8c57c9ac72
Jakub Tucholski <jtucholski00@gmail.com>
2022-01-03 12:53:22 -0800

    finished day 9

--- src/day_nine.rs
+++ src/day_nine.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, VecDeque};
 
 pub fn sum_low_points(pts: Vec<Vec<usize>>) -> usize {
     let mut sum = 0;
@@ -13,50 +13,51 @@ pub fn sum_low_points(pts: Vec<Vec<usize>>) -> usize {
     sum
 }
 
-pub fn find_basins(pts: Vec<Vec<usize>>) -> usize {
-    let mut basin_sizes: Vec<usize> = Vec::new();
+fn get_low_points(pts: &Vec<Vec<usize>>) -> Vec<(usize, usize)> {
+    let mut low_pts: Vec<(usize, usize)> = Vec::new();
 
     for (x, row) in pts.iter().enumerate() {
-        for (y, &elt) in row.iter().enumerate() {
-            let adjacent = get_adjacent(x, y, &pts);
-
-            if get_values(&adjacent, &pts).iter().all(|&pt| pt > elt) {
-                let mut basin_pts: HashSet<(usize, usize)> = HashSet::new();
-                // build map of adjacent
-                basin_pts.insert((x, y));
-
-                let mut to_process: Vec<(usize, usize)> = adjacent.clone();
-
-                while !to_process.is_empty() {
-                    let (next_x, next_y) = to_process.pop().unwrap();
-                    if basin_pts.contains(&(next_x, next_y)) {
-                        continue;
-                    }
-                    let cur_elt = pts[next_x][next_y];
-                    let cur_adjacent = get_adjacent(next_x, next_y, &pts);
-
-                    let mut not_processed: Vec<(usize, usize)> = cur_adjacent
-                        .into_iter()
-                        .filter(|elt| !basin_pts.contains(elt))
-                        .collect();
-
-                    if not_processed.is_empty() {
-                        
-                    }
-
-                    if !not_processed.is_empty()
-                        && get_values(&not_processed, &pts)
-                            .iter()
-                            .all(|&x| x > cur_elt)
-                    {
-                        basin_pts.insert((next_x, next_y));
-                        to_process.append(&mut not_processed);
-                    }
-                }
-                basin_sizes.push(basin_pts.len());
+        for (y, elt) in row.iter().enumerate() {
+            let adjacent_points = get_adjacent(x, y, &pts);
+            if get_values(&adjacent_points, &pts).iter().all(|pt| pt > elt) {
+                low_pts.push((x, y));
             }
         }
     }
+    low_pts
+}
+
+fn get_basin_size(pts: &Vec<Vec<usize>>, low_pt: (usize, usize)) -> usize {
+    let mut seen: HashSet<(usize, usize)> = HashSet::new();
+    let mut to_process: VecDeque<(usize, usize)> = VecDeque::from_iter([low_pt]);
+
+    while !to_process.is_empty() {
+        let (x, y) = to_process.pop_front().unwrap();
+        if seen.contains(&(x, y)) {
+            continue;
+        }
+
+        if pts[x][y] == 9 {
+            continue;
+        }
+
+        seen.insert((x, y));
+
+        let neighbors: Vec<(usize, usize)> = get_adjacent(x, y, pts);
+        for &pt in neighbors.iter() {
+            if !seen.contains(&pt) {
+                to_process.push_back(pt);
+            }
+        }
+    }
+    seen.len()
+}
+
+pub fn find_basins(pts: Vec<Vec<usize>>) -> usize {
+    //let mut basin_sizes: Vec<usize> = Vec::new();
+    let low_pts = get_low_points(&pts);
+
+    let mut basin_sizes: Vec<usize> = low_pts.iter().map(|&low_pt| get_basin_size(&pts, low_pt)).collect(); 
     basin_sizes.sort_unstable();
     basin_sizes
         .iter()
