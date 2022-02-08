1b170e7e5f44efcbfd453e04ebe969c8cba84870
Jakub Tucholski <jtucholski00@gmail.com>
2022-02-08 10:11:49 -0800

    checkpointing day 8 progress

--- src/day_eight.rs
+++ src/day_eight.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
 pub fn get_unique_digits(input: Vec<String>) -> usize {
     let mut appearances = 0;
@@ -14,6 +14,60 @@ pub fn get_unique_digits(input: Vec<String>) -> usize {
             .count();
     }
     appearances
+}
+
+pub fn determine_output_value(input: String) -> usize {
+    let digits_to_segments: Vec<HashSet<char>> = vec![
+        /*
+        (0, vec!['a','b','c','e','f','g'].into_iter().collect::<HashSet<char>>()),
+        (1, vec!['c','f'].into_iter().collect::<HashSet<char>>()),
+        (2, vec!['a','c','d','e','g'].into_iter().collect::<HashSet<char>>()),
+        (3, vec!['a','c','d','f','g'].into_iter().collect::<HashSet<char>>()),
+        (4, vec!['b','c','d','f'].into_iter().collect::<HashSet<char>>()),
+        (5, vec!['a','b','d','f','g'].into_iter().collect::<HashSet<char>>()),
+        (6, vec!['a','b','d','e','f','g'].into_iter().collect::<HashSet<char>>()),
+        (7, vec!['a','c','f'].into_iter().collect::<HashSet<char>>()),
+        (8, vec!['a','b','c','d','e','f','g'].into_iter().collect::<HashSet<char>>()),
+        (9, vec!['a','b','c','d','f','g'].into_iter().collect::<HashSet<char>>()),
+        */
+        vec!['a','b','c','e','f','g'].into_iter().collect::<HashSet<char>>(),
+        vec!['c','f'].into_iter().collect::<HashSet<char>>(),
+        vec!['a','c','d','e','g'].into_iter().collect::<HashSet<char>>(),
+        vec!['a','c','d','f','g'].into_iter().collect::<HashSet<char>>(),
+        vec!['b','c','d','f'].into_iter().collect::<HashSet<char>>(),
+        vec!['a','b','d','f','g'].into_iter().collect::<HashSet<char>>(),
+        vec!['a','b','d','e','f','g'].into_iter().collect::<HashSet<char>>(),
+        vec!['a','c','f'].into_iter().collect::<HashSet<char>>(),
+        vec!['a','b','c','d','e','f','g'].into_iter().collect::<HashSet<char>>(),
+        vec!['a','b','c','d','f','g'].into_iter().collect::<HashSet<char>>(),
+    ];
+    let mut possible_mappings: HashMap<char,HashSet<char>> = HashMap::new();
+    let mut definite_mappings: HashMap<char, char> = HashMap::new();
+
+    // determine possible mappings
+    let segments: Vec<&str> = input.split('|').collect();
+    if segments.len() != 2 {
+        return 0;
+    }
+
+    let all_digits: Vec<&str> = segments[0].split_ascii_whitespace().collect();
+    let output_digits: Vec<&str> = segments[1].split_ascii_whitespace().collect();
+    
+    for &digit in all_digits.iter() {
+        let possible: Vec<HashSet<char>> = digits_to_segments.iter().filter(|&set| digit.len() == set.len()).map(|set| set.clone()).collect();
+        for c in digit.chars() {
+            if definite_mappings.contains_key(&c) {
+                continue;
+            }
+            for &possible_item in possible.iter() {
+                if !possible_mappings.contains_key(&c) {
+                    possible_mappings.insert(c, possible_item);
+                }
+            let current_guesses: &HashSet<char> = possible_mappings.get(&c).unwrap_or(&HashSet::new());
+            }
+        }
+    }
+    0
 }
 
 #[cfg(test)]
